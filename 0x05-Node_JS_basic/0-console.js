53a51f0a9eceaabca3e331f915467592d2222e7a
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-08-19 12:34:21 +0200

    19-8-2024

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/SalwaMahmoud9>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
